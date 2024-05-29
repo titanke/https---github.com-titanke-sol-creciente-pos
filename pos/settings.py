920f763ed840bd359182e8d8f3d0efc334d1df1f
titanke <81938777+titanke@users.noreply.github.com>
2024-05-28 21:34:54 -0500

    cambios st

--- pos/settings.py
+++ pos/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/staticfiles/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_URL = '/media/'
